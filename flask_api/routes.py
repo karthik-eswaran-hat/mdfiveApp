0532f93fd7eb35902cdbb8fd65df2beda0576cc2
Krishna Prasath <krishnaprasath.thirunavukarasu@hashagile.com>
2025-07-28 18:35:37 +0530

    chart data

--- flask_api/routes.py
+++ flask_api/routes.py
@@ -20,31 +20,30 @@ def register_routes(app):
 
     @app.route('/api/test', methods=['GET'])
     def test():
-      try:
-          with db_cursor() as cur:
-              query = """
-                  SELECT * FROM test_suite.get_valid_report_sample_counts_app(
-                      (
-                          SELECT MAX(
-                              CAST(SUBSTRING(report_name FROM 'Report_(\\d+)_') AS INTEGER)
-                          )
-                          FROM test_suite.json_report_test_data_app
-                          WHERE report_name ~ 'Report_\\d+_'
-                      )
-                  );
-              """
-              cur.execute(query)
-              rows = cur.fetchall()
-          return jsonify({
-              "success": True,
-              "data": rows
-          })
-      except Exception as e:
-          return jsonify({
-              "success": False,
-              "error": str(e)
-          }), 500
-
+        try:
+            with db_cursor() as cur:
+                query = """
+                    SELECT * FROM test_suite.get_valid_report_sample_counts_app(
+                        (
+                            SELECT MAX(
+                                CAST(SUBSTRING(report_name FROM 'Report_(\\d+)_') AS INTEGER)
+                            )
+                            FROM test_suite.json_report_test_data_app
+                            WHERE report_name ~ 'Report_\\d+_'
+                        )
+                    );
+                """
+                cur.execute(query)
+                rows = cur.fetchall()
+            return jsonify({
+                "success": True,
+                "data": rows
+            })
+        except Exception as e:
+            return jsonify({
+                "success": False,
+                "error": str(e)
+            }), 500
 
     @app.route('/api/get-latest-report', methods=['GET'])
     def get_latest_report():
@@ -69,21 +68,35 @@ def register_routes(app):
 
     @app.route('/api/load-test-data', methods=['POST'])
     def load_test_data():
-        query = query = """
-                  SELECT * FROM test_suite.load_valid_sample_report_app(
-                      (
-                          SELECT MAX(
-                              CAST(SUBSTRING(report_name FROM 'Report_(\\d+)_') AS INTEGER)
-                          )
-                          FROM test_suite.json_report_test_data_app
-                          WHERE report_name ~ 'Report_\\d+_'
-                      )
-                  );
-              """
+        query = """
+            SELECT * FROM test_suite.load_valid_sample_report_app(
+                (
+                    SELECT MAX(
+                        CAST(SUBSTRING(report_name FROM 'Report_(\\d+)_') AS INTEGER)
+                    )
+                    FROM test_suite.json_report_test_data_app
+                    WHERE report_name ~ 'Report_\\d+_'
+                )
+            );
+        """
         try:
             with db_cursor() as cur:
                 cur.execute(query)
                 result = cur.fetchone()
             return jsonify({'success': True, 'result': result})
+        except Exception as e:
+            return jsonify({'success': False, 'error': str(e)}), 500
+
+    @app.route('/api/report-summary', methods=['GET'])
+    def get_report_combination_summary():
+        query = "SELECT * FROM test_suite.report_combination_summary;"
+        try:
+            with db_cursor() as cur:
+                cur.execute(query)
+                rows = cur.fetchall()
+            return jsonify({
+                'success': True,
+                'data': rows
+            })
         except Exception as e:
             return jsonify({'success': False, 'error': str(e)}), 500
