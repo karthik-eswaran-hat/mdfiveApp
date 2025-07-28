a2c316860d0810f436f19f9f3b5c74ea11d20bab
Krishna Prasath <krishnaprasath.thirunavukarasu@hashagile.com>
2025-07-28 18:10:48 +0530

    load_data

--- mdfive/src/api/project_report.ts
+++ mdfive/src/api/project_report.ts
@@ -1,10 +1,15 @@
 import service from "../lib/axios";
 
-
-
 export function getValidReport(){
     return service({
         url:"api/test",
         method:"get"
+    })
+}
+
+export function loadTestData(){
+    return service({
+        url:"api/load-test-data",
+        method:"post"
     })
 }
